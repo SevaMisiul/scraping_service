81266d56b109b56d75cef2b7359f1efe68efbc75
SevaMisiul <sevamisiul@gmail.com>
2022-10-23 18:13:23 +0300

    creating model language and vacancy

--- scraping/models.py
+++ scraping/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+from .utils import from_cyrillic_to_latin
+
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название города')
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name='Название города', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Город'
@@ -11,3 +13,43 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_latin(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_latin(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    requirement = models.TextField(verbose_name='Требования')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Язык программирования')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
